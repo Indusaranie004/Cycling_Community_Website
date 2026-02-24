3b1a06fd28dd4e41322b03ee3c27f66b8453e574
Yonika De Silva <yonikadesilva12@gmail.com>
2026-02-24 23:41:52 +0530

    Added communityChallengeRoutes.js

---
+++ backend/routes/communityChallengeRoutes.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+const {
+    getAllCommunityChallenges,
+    getCommunityChallengeById,
+    createCommunityChallenge,
+    joinCommunityChallenge,
+    updateCommunityChallengeProgress,
+    getCommunityChallengeLeaderboard,
+    getUserParticipationHistory,
+    checkChallengeEnded
+} = require('../controllers/communityChallengeController');
+
+// /api/community/challenges
+router.get('/', getAllCommunityChallenges);
+router.post('/', createCommunityChallenge);
+
+// /api/community/challenges/:id
+router.get('/:id', getCommunityChallengeById);
+router.get('/:id/leaderboard', getCommunityChallengeLeaderboard);
+router.post('/:id/join', joinCommunityChallenge);
+router.put('/:id/progress', updateCommunityChallengeProgress);
+router.post('/:id/check-ended', checkChallengeEnded);
+
+// /api/community/challenges/user/:userId
+router.get('/user/:userId', getUserParticipationHistory);
+
+module.exports = router;
