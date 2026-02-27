6c55bcd93af060857bfda17d27923096a0661d78
“Indusaranie” <cikhewa@gmail.com>
2026-02-27 11:09:32 +0530

    User management files synced from dev

--- backend/app.js
+++ backend/app.js
@@ -12,6 +12,7 @@ app.use((req, res, next) => {
 });
 
 app.use('/api/routes', require('./routes/routeRoutes'));
+app.use('/api/users', require('./routes/userRoutes'));
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
